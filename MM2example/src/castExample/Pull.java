5adde85d4cfc6134dfbf25c0c3edcffd5018b712
sineadmcd <s.mcdonagh24@nuigalway.ie>
2017-01-06 18:39:39 +0000

    removed bugbot fixes

--- MM2example/src/castExample/Pull.java
+++ MM2example/src/castExample/Pull.java
@@ -19,10 +19,7 @@ import java.net.URLConnection;
 		    String s1 ="hello";
 		    String==String;
 		    if(s == s1)
-[string].equals([string])
 
-\\equals method comparing different varTypes
-\\equals method comparing different varTypes
 		    int j=4;
 		    if(i==j)
 	        // Get the input stream through URL Connection
