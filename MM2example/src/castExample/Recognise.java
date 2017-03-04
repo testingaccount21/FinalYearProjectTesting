995114d5e08285b213b56905423a8987d8240d6d
sineadmcd <s.mcdonagh24@nuigalway.ie>
2017-03-04 21:41:45 +0000

    bugs

--- MM2example/src/castExample/Recognise.java
+++ MM2example/src/castExample/Recognise.java
@@ -18,81 +18,36 @@ public class Recognise {
 	
 	public static void main(String[] args)
 	{
-		System.out.println("BUG:  int f = (int)(Math.random()*101)*100; //ERROR: cast is higher priority than multiply");
-		System.out.println("-------------------------------------------------------------------------------------");
 		double testing = Math.random();
 		double testingt100 = testing*100;
+	        System.out.println("enter regex: ");
+	        	
+	            Pattern pattern =Pattern.compile(in.nextLine());
+	            
+	            System.out.println("enter string ");
+	            Matcher matcher =pattern.matcher(in.nextLine());
+
+	            boolean found = false;
+	            while (matcher.find())
+		    {
+	                System.out.println("I found the text '" +
+	                    matcher.group()+ "' starting at " + matcher.start() +
+	                    "index,and ending at index " + matcher.end());
+	                 
+	                found = true;
+	            }
+	            if(!found){
+	                System.out.println("No match found");
+		    }
 		
 		if(testingt100==Double.NaN)
 		{
-			//do stuff
-		}
-		int testingint = (int)testing;
-		int desired = (int)testingt100;
-		int actual = (int)testing*100;
-		System.out.println("Math.random() returns a number between 0 and 1 for example: " + testing + 
-							"\nThe coder was looking to to multiply this random double by 100 which would have returned: " + testingt100 +
-							"\nand then round it to the nearest lesser integer through casting: " + desired +
-							"\nbut because the cast has a higher priority what the program actually did was cast the Math.random() result to an integer first: " + testingint +
-							"\nwhich will always result in 0, and then multiply 0 by 100: " + actual +
-							"\n      i.e the coders random number is always 0");
-		
-		System.out.println("\n The bug my code should look for is (int)math.random() in that exact sequence with no other brackets etc \n" );
-		boolean yes=false;
-		boolean no= false;
-		if(yes.equals(testing))
-		if(yes=no)
-		{
-			//do stuff
-		}
-		try {
-			Read();
-		} catch (IOException e) {
-		
-		}
-	}
-	
-	public static void Read() throws IOException
-	{
-			String print = "null";
-			RandomAccessFile accessFile = new RandomAccessFile("TestRead.txt", "rw");
+			System.out.println("This is not a numbr");
+			bugCount++;
 			
-			for (int i=1; i<accessFile.length(); i++)
-			{
-				try{
-					 print = accessFile.readLine();
-					System.out.println(print);
-					
-					if ( !(print.equals(null)) & print.contains("(int)(Math.random()*101)"))
-\\equals method comparing different varTypes
-						BUG = true;
-						bugCollect.add(print); 
-						bugCount++;
-					}
-				}
-					
-				catch (NullPointerException e)
-				{
-						i = (int) (accessFile.length()*2);
-						Report();
-				}
-					
-			}
-
-	}
-	
-	public static void Report()
-	{
-		if(BUG)
-		{
-			System.out.println("There were " + bugCollect.size()+ " bugs found");
-			 for(int j = 0; j < bugCollect.size(); j++) {
-		            System.out.println(bugCollect.get(j));
-		        }
 		}
-		else
-		{
-			System.out.println("NO BUGS FOUND");
-		}
+		
+		
+		
 	}
 }
