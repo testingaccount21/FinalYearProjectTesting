ca620b48a499d0d02451aff5d7e64048f7f24667
sineadmcd <s.mcdonagh24@nuigalway.ie>
2017-03-04 21:38:20 +0000

    added bugs

--- MM2example/src/castExample/Pull.java
+++ MM2example/src/castExample/Pull.java
@@ -1,40 +1,26 @@
 package castExample;
 
+import java.io.BufferedReader;
 
-
-	import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 
 	public class Pull  {
+		private boolean predefined = false;
 	    public static void main(String[] args) throws IOException {
 			char[] chararray = new char[3];
 	        // Make a URL to the web page
 	        URL url = new URL("https://github.com/sineadmcd/FinalYearProjectTesting");
 		String s ="hi";
-		    String s1 ="hello";
-		    String==String;
-		    if(s == s1)
-
+		String s1 ="hello";
+		if(s == s1){
 		    int j=4;
-		    if(i==j)
 	        // Get the input stream through URL Connection
 	        URLConnection con = url.openConnection();
 	        InputStream is =con.getInputStream();
-
-	        // Once you have the Input Stream, it's just plain old Java IO stuff.
-
-	        // For this case, since you are interested in getting plain-text web page
-	        // I'll use a reader and output the text content to System.out.
-
-	        // For binary content, it's better to directly read the bytes from stream and write
-	        // to the target file.
-
-
+		System.out.println("enter boolean value")
+		Scanner in = new Scanner()
+			
+		boolean userin = in.readline();
+		if(usering=preefined){
 	        BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
 	        String line = null;
@@ -43,8 +29,9 @@ import java.net.URLConnection;
 	        while ((line = br.readLine()) != null) {
 	            System.out.println(line);
 	        }
+			j=j++;
+		}
 		    
-		    long x = (long)(Math.random()*101);
 	    }
 	
 }
