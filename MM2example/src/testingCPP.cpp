3db0bec1ce47f0b732f8cd4c2aa55f52c0a6cb12
sineadmcd <s.mcdonagh24@nuigalway.ie>
2017-03-05 19:10:02 +0000

    BugBot fixed

--- MM2example/src/testingCPP.cpp
+++ MM2example/src/testingCPP.cpp
@@ -1,6 +1,6 @@
 int main(){
 
-    double d=1/2;
+    double d=1.0/2;
     
     int i=3;
     int j=4;
@@ -8,6 +8,6 @@ int main(){
     if(3=4)
       printf("hello world!");
       
-      return i++;
+      i=i+1; \n return i;++;
       
       }
